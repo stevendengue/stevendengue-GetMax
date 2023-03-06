50d2425d5027e15aa54dfa558d6882fee3a599ad
Steve Ndengue <steve.ndengue@gmail.com>
2023-03-06 21:52:56 +0000

    Challenge: Get Max complete

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int maxval=arr[0];
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]>maxval){
+                maxval=arr[i];
+            }
+        }
+        return maxval;
     }
 }
